abd50bccc590385cd06d4bd7c5da150498830b56
ethany21 <imwoodam@hanmail.net>
2021-04-15 00:51:50 +0900

    make visited boolean array global and added as parameter into dfs function

--- DFS/BOJ1325.py
+++ DFS/BOJ1325.py
@@ -17,9 +17,12 @@ for i in range(1, node+1):
     # dic = {1:[1], 2:[2], 3:[3] ... 5:[5]}
     dic[i] = [i]
 
+# 기존엔 visited가 dfs 함수 안에 있었다면, 이를 전역 변수화 하고 dfs 파라미터에 추가했다
+# cycle이 입력으로 들어 올 시는 해결되었다 (아닐수도.....)
+visited = [False for _ in range(edge)]
+
 # 백트래킹 방식으로 위에서 입력으로 받아오고 만든 graph를 순회한다
-def dfs(graph, first, original):
-    visited = [False for _ in range(edge)]
+def dfs(graph, first, original, visited):
     count = 0
     for i in visited:
         if i == True:
@@ -34,13 +37,13 @@ def dfs(graph, first, original):
             # dic[original] += 1
             first = graph[i][0]
             visited[i] = True
-            dfs(graph, first, original)
+            dfs(graph, first, original, visited)
             first = graph[i][1]
             visited[i] = False
 
 # node 개수의 컴퓨터만큼 순회를 돌면서, dictionay에 각 key node마다 해킹할 수 있는 컴퓨터를 append한다
 for i in range(1, node+1):
-    dfs(graph, i, i)
+    dfs(graph, i, i, visited)
 
 # 해킹 가능한 컴퓨터의 개수 중 가장 최댓값을 찾는다
 max = 0
