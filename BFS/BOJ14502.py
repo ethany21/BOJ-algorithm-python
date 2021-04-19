881516a28c6eb45708a4f68004407b25eb771b01
ethany21 <imwoodam@hanmail.net>
2021-04-19 23:49:51 +0900

    Adde BOJ14502

---
+++ BFS/BOJ14502.py
@@ -0,0 +1,32 @@
+from itertools import combinations
+from collections import deque
+
+N, M = map(int, input().split())
+
+graph = []
+count = 0
+cands = []
+cords = []
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+for _ in range(M):
+    graph.append(list(map(int, input().split())))
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            count += 1
+            cords.append([i, j])
+
+cands = list(combinations(cords, 3))
+
+for i in range(cands):
+    queue = deque()
+
+
+
+
+
