9672b473afa04a5cbc82e0917877956ce031d6d1
ethany21 <imwoodam@hanmail.net>
2021-04-22 04:33:42 +0900

    Completed BOJ14502

--- BFS/BOJ14502.py
+++ BFS/BOJ14502.py
@@ -12,7 +12,7 @@ dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-for _ in range(M):
+for _ in range(N):
     graph.append(list(map(int, input().split())))
 
 for i in range(N):
@@ -23,15 +23,24 @@ for i in range(N):
         elif graph[i][j] == 2:
             walls.append([i, j])
 
+
+
 cands = list(combinations(cords, 3))
-answers = [count for _ in range(len(cands))]
+answers = [0 for _ in range(len(cands))]
 
-for comb in range(cands):
+for comb in range(len(cands)):
+    
+    queue = deque()
+    for i in walls:
+        queue.append(i)
+    
+    box = [[0 for _ in range(M)] for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            box[i][j] = graph[i][j]
 
-    queue = deque(walls)
-
-    for cord in comb:
-        graph[cord[0]][cord[1]] = 1
+    for cord in cands[comb]:
+        box[cord[0]][cord[1]] = 1
 
     while queue:
         temp = []
@@ -42,13 +51,15 @@ for comb in range(cands):
             for i in range(4):
                 x = a + dx[i]
                 y = b + dy[i]
-                if 0 <= x < N and 0 <= y < M and graph[x][y] == 0:
+                if 0 <= x < N and 0 <= y < M and box[x][y] == 0:
                     queue.append([x,y])
-                    answers[comb] -=1
-                    graph[x][y] = 2
+                    box[x][y] = 2
+    for i in range(N):
+        for j in range(M):
+            if box[i][j] == 0:
+                answers[comb] += 1
+print(max(answers))
         
-    
-
 
 
 
