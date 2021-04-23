d0898f91d1386f79594c95af8dd7cb7add3746e7
ethany21 <imwoodam@hanmail.net>
2021-04-23 23:57:53 +0900

    Modified BOJ15486

--- BFS/BOJ16234.py
+++ BFS/BOJ16234.py
@@ -1,6 +1,12 @@
+from collections import deque
+
 N, L, R = map(int, input().split())
 
 populations = []
+queue = deque()
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 
 for _ in range(N):
     populations.append(list(map(int, input().split())))
+
