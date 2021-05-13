da668a7bf8c72744bdbefff76c62dbcc79ca7b27
ethany21 <imwoodam@hanmail.net>
2021-05-13 23:48:47 +0900

    Modified BOJ2661 to use check method

--- Backtracking/BOJ2661.py
+++ Backtracking/BOJ2661.py
@@ -1,9 +1,27 @@
-N = int(input())
+# N = int(input())
 
 array = [1, 2, 3]
 visited = [False for _ in range(3)]
 
 cand = []
 
-def dfs(visited, N):
+## temp는 리스트로, 쌓으면서 앞 원소들과 비교해 본다
+def dfs(visited, N, count, temp):
     pass
+
+def check(temp):
+
+    result = False
+
+    for i in range(int(len(temp)/2)):
+        front = temp[-i*2:-i]
+        back = temp[-i:-1]
+        if front == back:
+            result = True
+            break
+
+    return result
+
+print(int(3/2))
+print(check([1,3,3]))
+
