075734e7f7c0e918fb2cdfb14e757cc3b7fbf8a9
ethany21 <imwoodam@hanmail.net>
2021-05-17 23:57:23 +0900

    Modified BOJ2667 for BFS

--- BFS/BOJ2667.py
+++ BFS/BOJ2667.py
@@ -14,3 +14,12 @@ queue.append([0,0])
 
 while queue:
     temp = []
+    while len(queue) > 0:
+        temp.append(queue.popleft)
+    for cord in temp:
+        a , b = cord[0], cord[1]
+        for i in range(4):
+            x = a + dx[i]
+            y = b + dy[i]
+            if 0 <= x < N and 0 <= y < N and graph[x][y] == 1:
+                queue.append([x,y])
