3f7a62650cbedcae7a0099970fb79de135fa37a5
ethany21 <imwoodam@hanmail.net>
2021-05-18 23:49:19 +0900

    Modified BOJ2667

--- BFS/BOJ2667.py
+++ BFS/BOJ2667.py
@@ -3,6 +3,7 @@ from collections import deque
 N = int(input())
 graph = []
 answer = []
+visited = []
 queue = deque()
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -10,12 +11,17 @@ dy = [0, 0, -1, 1]
 for _ in range(N):
     graph.append(list(map(int, input().split())))
 
+for _ in range(N):
+    visited.append([False for _ in range(N)])
+
 queue.append([0,0])
 
 while queue:
     temp = []
+    count = 0
     while len(queue) > 0:
         temp.append(queue.popleft)
+        answer.append(count)
     for cord in temp:
         a , b = cord[0], cord[1]
         for i in range(4):
@@ -23,3 +29,5 @@ while queue:
             y = b + dy[i]
             if 0 <= x < N and 0 <= y < N and graph[x][y] == 1:
                 queue.append([x,y])
+                count +=1
+print(answer)
