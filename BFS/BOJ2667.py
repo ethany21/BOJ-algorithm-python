5d025f1359631b9b45efec4d4920e797de8ff1b5
ethany21 <imwoodam@hanmail.net>
2021-05-21 00:03:50 +0900

    Modified BOJ2667 for BFS

--- BFS/BOJ2667.py
+++ BFS/BOJ2667.py
@@ -4,7 +4,6 @@ N = int(input())
 graph = []
 answer = []
 visited = []
-queue = deque()
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -16,11 +15,11 @@ for _ in range(N):
 
 for i in range(N):
     for j in range(N):
-        visited[i][j] == True
-        if graph[i][j] == 1:
+        if graph[i][j] == 1 and not visited[i][j]:
+            visited[i][j] = True
             queue = deque()
             queue.append([i, j])
-            count = 0
+            count = 1
             while queue:
                 temp = []
                 while len(queue) > 0:
@@ -30,9 +29,12 @@ for i in range(N):
                     for i in range(4):
                         x = a + dx[i]
                         y = b + dy[i]
-                        if 0 <= x < N and 0 <= y < N and graph[x][y] == 1 and not visited[a][b]:
-                            graph[a][b] = True
+                        if 0 <= x < N and 0 <= y < N and graph[x][y] == 1 and not visited[x][y]:
+                            visited[x][y] = True
                             queue.append([x, y])
                             count +=1
             answer.append(count)
-print(answer)
+answer.sort()
+print(len(answer))
+for i in answer:
+    print(i)
