5013c701b57eb30d3b5c5a6ccd4232fe6ef5f4df
ethany21 <imwoodam@hanmail.net>
2021-05-20 23:17:13 +0900

    Modified BOJ2667 with BFS

--- BFS/BOJ2667.py
+++ BFS/BOJ2667.py
@@ -14,20 +14,25 @@ for _ in range(N):
 for _ in range(N):
     visited.append([False for _ in range(N)])
 
-queue.append([0,0])
-
-while queue:
-    temp = []
-    count = 0
-    while len(queue) > 0:
-        temp.append(queue.popleft)
-        answer.append(count)
-    for cord in temp:
-        a , b = cord[0], cord[1]
-        for i in range(4):
-            x = a + dx[i]
-            y = b + dy[i]
-            if 0 <= x < N and 0 <= y < N and graph[x][y] == 1:
-                queue.append([x,y])
-                count +=1
+for i in range(N):
+    for j in range(N):
+        visited[i][j] == True
+        if graph[i][j] == 1 and not visited[i][j]:
+            queue = deque()
+            queue.append([i, j])
+            count = 0
+            while queue:
+                temp = []
+                while len(queue) > 0:
+                    temp.append(queue.popleft())
+                for cord in temp:
+                    a , b = cord[0], cord[1]
+                    for i in range(4):
+                        x = a + dx[i]
+                        y = b + dy[i]
+                        if 0 <= x < N and 0 <= y < N and graph[x][y] == 1 and not visited[a][b]:
+                            graph[a][b] = True
+                            queue.append([x, y])
+                            count +=1
+            answer.append(count)
 print(answer)
