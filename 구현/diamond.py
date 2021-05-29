8e50eba658999a93303a59ce1d89813c48e7c86e
ethany21 <imwoodam@hanmail.net>
2021-05-29 14:57:14 +0900

    Completed Diamond problem

--- 구현/diamond.py
+++ 구현/diamond.py
@@ -1,4 +1,7 @@
 n = int(input())
 
 for i in range(n):
-    print( " " * (n-2*i)+ "*" * (2*i + 1))
+    if int((n-(2*i+1))/2) >= 0:
+        print(" " * int((n-(2*i+1))/2)+"*" * (2*i+1)+" " * int((n-(2*i+1))/2))
+    else:
+        print(" "*int((2*i+2-n)/2)+"*"*(2*n - (2*i+1))+" "*int((2*i+2-n)/2))
