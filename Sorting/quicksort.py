b8eead3c1bd3e83b43797c8eca63caa2978aee32
ethany21 <imwoodam@hanmail.net>
2021-06-01 02:41:07 +0900

    implemented quicksort without cache

--- Sorting/quicksort.py
+++ Sorting/quicksort.py
@@ -14,5 +14,31 @@ def quick_sort(arr):
             equal.append(i)
     return quick_sort(less) + equal + quick_sort(more)
 
+def partition(arr, start, end):
+    pivot = arr[start]
+    left = start + 1
+    right = end
+
+    done = False
+    while not done:
+        while left <= right and arr[left] <= pivot:
+            left += 1
+        while left <= right and pivot <= arr[right]:
+            right -= 1
+        if left > right:
+            done = True
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+    arr[start], arr[right] = arr[right], arr[start]
+    return right
+
+def quicksort(arr, start, end):
+    if start < end:
+        pivot = partition(arr, start, end)
+        quicksort(arr, start, pivot-1)
+        quicksort(arr, pivot+1, end)
+    return arr
+
+print(quicksort(lst, 0, len(lst) - 1))
 
 print(quick_sort(lst))
