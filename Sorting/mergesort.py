9174f3b2ad7ec3bf242068dc54cf3759712df66f
ethany21 <imwoodam@hanmail.net>
2021-05-31 23:58:39 +0900

    devided sorting and merge

--- Sorting/mergesort.py
+++ Sorting/mergesort.py
@@ -28,6 +28,41 @@ def mergeSort_ASC(arr):
             arr[k] = right_half[j]
             j += 1
             k += 1
+def merge_sorted(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        l = merge_sorted(left)
+        r = merge_sorted(right)
+
+        return merge(l, r)
+
+    else:
+        return arr
+        
+def merge(left, right):
+    i,j = 0, 0
+    arr = []
+
+    while i <len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr.append(left[i])
+            i += 1
+        else:
+            arr.append(right[j])
+            j += 1
+    while i < len(left):
+        arr.append(left[i])
+        i += 1
+    while j < len(right):
+        arr.append(right[j])
+        j += 1
+
+    return arr
+
+
 print(lst)
 mergeSort_ASC(lst)
 print("Sorted Result is : " , lst)
