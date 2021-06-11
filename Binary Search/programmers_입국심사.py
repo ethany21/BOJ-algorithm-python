218502882547101f89770ef3ba624ceb85e95452
ethany21 <imwoodam@hanmail.net>
2021-06-11 23:57:44 +0900

    modified programmers_입국심사

--- Binary Search/programmers_입국심사.py
+++ Binary Search/programmers_입국심사.py
@@ -1,6 +1,10 @@
 def solution(n, times):
+
+    left, right = 1, max(times) * n
     answer = 0
     
-    
+    while left <= right:
+        mid = (left + right)//2
+        people = 0
     
     return answer
