cc47319836944c0014b855d722d33bd6f67a6231
ethany21 <imwoodam@hanmail.net>
2021-06-24 10:17:21 +0900

    implemented BOJ16234

--- BFS/BOJ16234.py
+++ BFS/BOJ16234.py
@@ -1,29 +1,46 @@
 from collections import deque
 
 N, L, R = map(int, input().split())
-
 populations = []
-queue = deque()
-visited = []
+
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
 for _ in range(N):
     populations.append(list(map(int, input().split())))
 
-for _ in range(N):
-    visited.append([False for _ in range(N)])
+def bfs(i, j):
+    visited[i][j] = 1
+    q = deque()
+    q.append([i,j])
+    temp = []
+    temp.append([i, j])
 
-def check(current):
-
-    adjacent = []
-
-    return adjacent
-
-def bfs():
-    pass
-
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            a = x + dx[i]
+            b = y + dy[i]
+            if 0 <= a < N and 0 <= b < N and visited[a][b] == 0:
+                if L <= abs(populations[a][b] - populations[x][y]) <= R:
+                    visited[a][b]= 1
+                    q.append([a,b])
+                    temp.append([a,b])
+    return temp
+cnt = 0
 while True:
+    visited = [[0]*N for _ in range(N)]
+    flag = False
     for i in range(N):
         for j in range(N):
-            pass
+            if visited[i][j] == 0:
+                temp = bfs(i,j)
+                if len(temp) > 1:
+                    flag = True
+                    num = sum(populations[x][y] for x,y in temp)//len(temp)
+                    for x, y in temp:
+                        populations[x][y] = num
+    if flag == False:
+        break
+    cnt +=1
+print(cnt)
